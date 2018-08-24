b30c39c5c06a257d15acf3f17ceba4e550ad4d63
haoqiyao <31806083+haoqiyao@users.noreply.github.com>
2018-08-23 22:52:58 -0400

    Update tree.py

--- HW2/tree/tree.py
+++ HW2/tree/tree.py
@@ -12,33 +12,51 @@ class Tree(object):
 
     def height(self, root):
         if not root:
+            #if empty tree, return 0
             return 0
+        #if not, recursively go to the left and right part, add 1 to the height
         return 1 + max(self.height(root.left), self.height(root.right))
 
     def print_tree(self, root):
+        #get the height of the tree
         height = self.height(root)
+        
+        #width of the array follows this structure
         width = 2 ** height - 1
 
         result = []
+        
+        #first initialize all the result as "|"
         for h in range(height):
             result.append(["|"] * width)
-
+        
+        #using bfs and update the corresponding values using array, start from root
         track = [(root, 0, 0, width)]
 
         for curr in track:
-
+            
+            #current node is root
             curr_node = curr[0]
+            #current level is 0
             curr_level = curr[1]
+            #current left is 0
             curr_l = curr[2]
+            #current right is width
             curr_r = curr[3]
-
+            
+            #get the middle index value 
             mid = int(np.floor((curr_l + curr_r)/2))
-
+            
+            #replace the result matrix with the node
             result[curr_level][mid] = str(curr_node.value)
-
+            
+            #if has left node, do left part again using bfs
             if curr_node.left:
+                #current node becomes left, level goes down by 1, current left stays the same, current right becomes middle
                 track.append((curr_node.left, curr_level + 1, curr_l, mid))
+            #same for right part
             if curr_node.right:
+                #current node becomes right, level goes down by 1, current left becomes middle, current right stays the same
                 track.append((curr_node.right, curr_level + 1, mid, curr_r))
 
         return np.mat(result)
